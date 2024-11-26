8391f35fb21e60c12bda84edb592e5d6e72847c6
Miroslav Petrov <114015697+MiroslavPetrovv@users.noreply.github.com>
2024-11-26 08:15:50 +0200

    Update

---
+++ JobApplications/Areas/HR/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿namespace JobApplications.Areas.HR.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    [Area("Recruiter")]
+    //[Route("/Admin/[controller]/[Action]/{id?}")]
+    [Authorize(Roles = "Company")]
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ JobApplications/Areas/HR/Controllers/CompanyRecrtuiterController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using JobApplications.Services.Interfaces;
+
+namespace JobApplications.Areas.HR.Controllers
+{
+    public class CompanyRecrtuiterController : BaseController
+    {
+        private IMapper mapper;
+        private readonly ICompanyService companyService;
+        private readonly IJobService jobService;
+        private readonly IIndustrieService industries;
+
+        public CompanyRecrtuiterController(IMapper mappingProfile, ICompanyService companyService,
+            IJobService jobService, IIndustrieService industrieService)
+        {
+
+            mapper = mappingProfile;
+            this.companyService = companyService;
+            this.jobService = jobService;
+            this.industries = industrieService;
+        }
+    }
+}
